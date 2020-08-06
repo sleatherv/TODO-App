af1f4e24e98823ca415897eb1a31ab8f4e14cf53
Sleather Vega <sleatherv28@hotmail.com>
2020-08-05 20:26:01 -0500

    Aprendimos a guardar en cookies y a redireccionar a otra ruta

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, make_response
 
 app = Flask(__name__)
 
-
+#creamos una nueva ruta raiz
 @app.route('/')
-def hello():
+def index():
+    #obtenemos la ip del usuario
     user_ip = request.remote_addr
+#   usamos make_reponse y redirect a hello
+    response = make_response(redirect('/hello'))
+    #agregamos la ip del usuario a una cookie 
+    response.set_cookie('user_ip', user_ip)
+    #regresamos la respuesta de flask
+    return response
 
+
+@app.route('/hello')
+def hello():
+    #obtenemos la ip del usuario desde la cookie
+    user_ip = request.cookies.get('user_ip')
     return f'Hello World Flask, tu IP es {user_ip}'
